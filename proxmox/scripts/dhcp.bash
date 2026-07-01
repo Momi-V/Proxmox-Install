b6df6c5d594df5af149de43a09cd714ac7bed2f4
Momi-V <83947761+Momi-V@users.noreply.github.com>
2026-07-02 00:52:50 +0200

    copy over scripts
    Copy over scripts from https://github.com/Momi-V/Notizen/tree/main/proxmox
    
    preseed.cfg references some of these and updating them was missed last time. They'll be brought up to date in a later commit

---
+++ proxmox/scripts/dhcp.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cp /etc/network/interfaces /etc/network/interfaces.bak
+sed -n '1h;1!H;${g;s/iface vmbr0.*10.1/iface vmbr0 inet dhcp/;p;}' /etc/network/interfaces.bak > /etc/network/interfaces
+
+VAR=$(cat <<'EOL'
+
+iface enp9s0 inet manual
+auto vmbr0
+iface vmbr0 inet dhcp
+        hwaddress ether DE:AD:2B:AD:00:99
+        bridge-ports enp9s0
+        bridge-stp off
+        bridge-fd 0
+
+EOL
+)
+
+VAR=$(VAR=${VAR@Q}; echo "${VAR:2:-1}")
+sed -i -E "s+iface enp9s0 inet (auto|dhcp)+$VAR+g" /etc/network/interfaces
+
+cat /etc/network/interfaces
---
+++ proxmox/scripts/pcie.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cat <<'EOL' > /etc/modules-load.d/vfio.conf
+vfio
+vfio_iommu_type1
+vfio_pci
+vfio_virqfd
+EOL
+cat /etc/modules-load.d/vfio.conf
+
+PCI=$(lspci -n -s 0e:00 | awk -F ': ' {'print $2'} | awk -F ' ' {'print $1'})
+echo "options vfio-pci ids=$(echo $PCI | sed 's/ /,/g') disable_vga=1" > /etc/modprobe.d/vfio.conf
+cat /etc/modprobe.d/vfio.conf
+
+echo "options kvm ignore_msrs=1 report_ignored_msrs=0" > /etc/modprobe.d/kvm.conf
+cat /etc/modprobe.d/kvm.conf
+
+sed -i -E 's/(GRUB_CMDLINE_LINUX_DEFAULT=".*)"/\1 amd_iommu=on iommu=pt pcie_acs_override=downstream,multifunction"/g' /etc/default/grub
+sed -i -E 's/#GRUB_TERMINAL=console/GRUB_TERMINAL=console/g' /etc/default/grub
+cat /etc/default/grub
+
+update-initramfs -u -k all
+update-grub
